4fd308caa15ec37f6582576c5cad259aac9ec9dc
Ethan Green <greenmarleyethan@gmail.com>
2018-01-04 02:04:09 -0500

    Application logging

--- src/Automaton/App.xaml.cs
+++ src/Automaton/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using Alphaleonis.Win32.Filesystem;
+using System;
+using System.Runtime.ExceptionServices;
+using System.Windows;
 
 namespace Automaton
 {
@@ -7,5 +10,22 @@ namespace Automaton
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var loggingPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+
+            if (!File.Exists(loggingPath))
+            {
+                File.Create(loggingPath).Dispose();
+            }
+
+            File.AppendAllText(loggingPath, $"{Environment.NewLine}{DateTime.Now} - Automaton Initiated");
+
+            AppDomain.CurrentDomain.FirstChanceException += (object source, FirstChanceExceptionEventArgs e) =>
+            {
+                File.AppendAllText(loggingPath, $"{Environment.NewLine}{DateTime.Now} - {e.Exception.StackTrace} {Environment.NewLine}--> {e.Exception.Message}");
+            };
+
+        }
     }
 }
