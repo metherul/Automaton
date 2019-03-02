82e9196b96927a4b166bdc386cec885c240a85d7
metherul <greenmarleyethan@gmail.com>
2019-03-02 05:09:53 -0500

    Expanded nexus login functions

--- src/Automaton.Model/NexusApi/ApiBase.cs
+++ src/Automaton.Model/NexusApi/ApiBase.cs
@@ -1,17 +1,36 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Authentication;
 using System.Threading.Tasks;
+using Automaton.Model.NexusApi.Interfaces;
+using Newtonsoft.Json.Linq;
 using WebSocketSharp;
 
 namespace Automaton.Model.NexusApi
 {
-    public class ApiBase
+    public class ApiBase : IApiBase
     {
+        public delegate void HasLoggedIn();
+
+        public event HasLoggedIn HasLoggedInEvent;
+
         private HttpClient HttpClient { get; set; }
 
         public string ApiKey { get; set; }
         public string GameName { get; set; }
+
+        public int RemainingDailyRequests { get; set; }
+
+        protected bool IsPremium { get; set; }
+        protected bool IsLoggedIn { get; set; }
+
+        public async Task<bool> InitializeAsync(string gameName, string apiKey = "")
+        {
+            return await Task.Factory.StartNew(() => Initialize(gameName, apiKey));
+        }
 
         public bool Initialize(string gameName, string apiKey = "")
         {
@@ -26,6 +45,18 @@ namespace Automaton.Model.NexusApi
             if (apiKey != string.Empty)
             {
                 ApiKey = apiKey;
+
+                HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpClient.DefaultRequestHeaders.Add("APIKEY", ApiKey);
+
+                // Get the premium status of the account
+                var response = HttpClient.GetAsync("/v1/users/validate.json").Result;
+                IsPremium = (bool)JObject.Parse(response.Content.ReadAsStringAsync().Result)["is_premium"];
+                IsLoggedIn = true;
+
+                RemainingDailyRequests = Convert.ToInt32(response.Headers.GetValues("X-RL-Daily-Remaining").ToList().First());
+
+                HasLoggedInEvent.Invoke();
             }
 
             else
@@ -37,6 +68,29 @@ namespace Automaton.Model.NexusApi
                 };
 
 
+                websocket.OnMessage += (sender, args) =>
+                {
+                    if (args == null || string.IsNullOrEmpty(args.Data)) return;
+
+                    ApiKey = args.Data;
+
+                    HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpClient.DefaultRequestHeaders.Add("APIKEY", ApiKey);
+
+                    // Get the premium status of the account
+                    var response = HttpClient.GetAsync("/v1/users/validate.json").Result;
+                    IsPremium = (bool)JObject.Parse(response.Content.ReadAsStringAsync().Result)["is_premium"];
+                    IsLoggedIn = true;
+
+                    RemainingDailyRequests = Convert.ToInt32(response.Headers.GetValues("X-RL-Daily-Remaining").ToList().First());
+
+                    HasLoggedInEvent.Invoke();
+                };
+
+                websocket.Connect();
+                websocket.Send("{\"id\": \"" + guid + "\", \"appid\": \"The Automaton Framework\"}");
+
+                Process.Start($"https://www.nexusmods.com/sso?id={guid}");
             }
 
             return false;
--- src/Automaton.ViewModel/NexusLoginViewModel.cs
+++ src/Automaton.ViewModel/NexusLoginViewModel.cs
@@ -1,15 +1,35 @@
-﻿using Automaton.ViewModel.Interfaces;
+﻿using Autofac;
+using Automaton.Model.Install.Intefaces;
+using Automaton.Model.NexusApi.Interfaces;
+using Automaton.ViewModel.Controllers.Interfaces;
+using Automaton.ViewModel.Interfaces;
 using GalaSoft.MvvmLight.Command;
 
 namespace Automaton.ViewModel
 {
     public class NexusLoginViewModel : ViewModelBase, INexusLoginViewModel
     {
+        private readonly IApiBase _apiBase;
+        private readonly IInstallBase _installBase;
+        private readonly IViewController _viewController;
+
         public RelayCommand LoginToNexusCommand => new RelayCommand(LoginToNexus);
 
-        public void LoginToNexus()
+        public NexusLoginViewModel(IComponentContext components)
         {
+            _apiBase = components.Resolve<IApiBase>();
+            _installBase = components.Resolve<IInstallBase>();
+            _viewController = components.Resolve<IViewController>();
+        }
 
+        public async void LoginToNexus()
+        {
+            _apiBase.HasLoggedInEvent += () =>
+            {
+                _viewController.IncrementCurrentViewIndex();
+            };
+
+            await _apiBase.InitializeAsync(_installBase.ModpackHeader.TargetGame);
         }
     }
 }
