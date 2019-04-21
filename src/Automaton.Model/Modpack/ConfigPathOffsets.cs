e813de99ff637055dc5bd86dca26c09ac16ae4f9
metherul <greenmarleyethan@gmail.com>
2019-04-20 23:44:23 -0400

    Added wildcards instead of string interpolation

--- src/Automaton.Model/Modpack/ConfigPathOffsets.cs
+++ src/Automaton.Model/Modpack/ConfigPathOffsets.cs
@@ -5,8 +5,8 @@
         public readonly string PackDefinitionConfig = "pack.auto_definition";
         public readonly string InstallConfig = "mods/{0}/install.json";
 
-        public readonly string SettingsIni = "{0}.ini";
-        public readonly string MetaIni = "mods/{0}/meta.ini";
+        public readonly string SettingsIni = "*.ini";
+        public readonly string MetaIni = "mods/*/meta.ini";
 
         public readonly string Plugins = "plugins.txt";
         public readonly string Modlist = "modlist.txt";
